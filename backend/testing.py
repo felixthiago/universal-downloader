36ff70210c05e32bc9f7a1d3c50ed76e9f2a57bf
felixthiago <thiagooliveira5427@gmail.com>
2025-07-27 00:16:31 -0300

    first commit

--- backend/testing.py
+++
@@ -1,8 +0,0 @@
-from platforms.ytdlp import stream_media
-from core.aws import upload_media
-
-res = stream_media("https://x.com/beingblackie_/status/1949023124877705524")
-
-url = upload_media(res)
-print(url)
-print(f"Url achada e deletando arquivo em 60 segundos")
