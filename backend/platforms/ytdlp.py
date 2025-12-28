3a474dfbb45a092410919ab51d82960949e9eafb
felixthiago <thiagooliveira5427@gmail.com>
2025-12-28 19:23:29 -0300

    'commited'

--- backend/platforms/ytdlp.py
+++ backend/platforms/ytdlp.py
@@ -36,6 +36,7 @@ def stream_media(url):
                 "verbose": True,
                 "noplaylist": True,
                 'nocheckcertificate': True,
+                'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
                 "extractor_args": {
                     "youtube": {
                         "formats": "missing_port"
